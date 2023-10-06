114c342bb6549a6825cde25935d878e599260a41
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-06 12:16:09 -0700

    all file needed created

---
+++ app/Http/Controllers/ListaCompraController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListaCompra;
+use App\Http\Requests\StoreListaCompraRequest;
+use App\Http\Requests\UpdateListaCompraRequest;
+
+class ListaCompraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreListaCompraRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ListaCompra $listaCompra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ListaCompra $listaCompra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateListaCompraRequest $request, ListaCompra $listaCompra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ListaCompra $listaCompra)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProdutoCategoriaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProdutoCategoria;
+use App\Http\Requests\StoreProdutoCategoriaRequest;
+use App\Http\Requests\UpdateProdutoCategoriaRequest;
+
+class ProdutoCategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProdutoCategoriaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProdutoCategoria $produtoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProdutoCategoria $produtoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProdutoCategoriaRequest $request, ProdutoCategoria $produtoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProdutoCategoria $produtoCategoria)
+    {
+        //
+    }
+}
