f55a846ab6081fb9087e72cda45ec6c1384eb588
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-06 12:30:14 -0700

    produtos categorias migration done

--- database/migrations/2023_10_06_191544_create_produto_categorias_table.php
+++ database/migrations/2023_10_06_191544_create_produto_categorias_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produto_categorias', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('produtos_categorias', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_produto');
+            $table->unsignedBigInteger('id_categoria');
+            $table->foreign('id_produto')->references('id')->on('produtos');
+            $table->foreign('id_categoria')->references('id')->on('categorias');
         });
     }
 
