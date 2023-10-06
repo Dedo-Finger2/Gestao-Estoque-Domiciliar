912c26313d092d5c78d3b9b14b36174f7a202382
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-06 12:31:56 -0700

    log despesas migration done

--- database/migrations/2023_10_06_191554_create_log_despesas_table.php
+++ database/migrations/2023_10_06_191554_create_log_despesas_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('log_despesas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id_despesa');
+            $table->double('valor_pago', 10, 2)->nullable(false);
+            $table->date('data_pagamento')->nullable(false);
         });
     }
 
