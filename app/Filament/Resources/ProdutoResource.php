798cccc13e38669298b996a0e559a371aaa56121
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-07 04:00:25 -0700

    filament beggining, user example

---
+++ app/Filament/Resources/ProdutoResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProdutoResource\Pages;
+use App\Filament\Resources\ProdutoResource\RelationManagers;
+use App\Models\Produto;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProdutoResource extends Resource
+{
+    protected static ?string $model = Produto::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProdutos::route('/'),
+            'create' => Pages\CreateProduto::route('/create'),
+            'edit' => Pages\EditProduto::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Policies/CategoriaPolicy.php
+++ app/Policies/CategoriaPolicy.php
@@ -13,7 +13,7 @@ class CategoriaPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class CategoriaPolicy
      */
     public function view(User $user, Categoria $categoria): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class CategoriaPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class CategoriaPolicy
      */
     public function update(User $user, Categoria $categoria): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class CategoriaPolicy
      */
     public function delete(User $user, Categoria $categoria): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class CategoriaPolicy
      */
     public function restore(User $user, Categoria $categoria): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class CategoriaPolicy
      */
     public function forceDelete(User $user, Categoria $categoria): bool
     {
-        //
+        return true;
     }
 }
